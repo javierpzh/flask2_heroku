f8947903300a38e909efd8ee1c7ffa15ec2e65b4
Javier Pérez Hidalgo <reyole111@gmail.com>
2020-04-20 16:32:44 +0200

    cambios  final app.py

--- app.py
+++ app.py
@@ -24,4 +24,5 @@ def categoria(categoria):
 	        return render_template("categorias.html",libros=datos,categoria=categoria)
     abort(404)
 
-app.run(debug=False)
+port=os.environ["PORT"]
+app.run('0.0.0.0',int(port),debug=False)
